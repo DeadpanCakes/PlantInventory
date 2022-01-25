1ecab9d3b6e05b604154291bb72c7604eb71992e
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-01-24 17:46:18 -0800

    made model

---
+++ models/item.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  name: { required: true, type: String },
+  price: { required: true, type: Number },
+  desc: { required: true, type: String, maxLength: 300 },
+  stock: { required: true, type: Number },
+  category: [{ types: Schema.Types.ObjectId, ref: "Category" }],
+});
+
+ItemSchema.virtual("url").get(function () {
+  return `/items/${this._id}`;
+});
+
+module.exports = mongoose.model("Item", ItemSchema);
