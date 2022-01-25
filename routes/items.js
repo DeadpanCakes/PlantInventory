28739cda1372e1372232c45823d9adff6815f0ca
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-01-25 15:49:46 -0800

    made routes for items

---
+++ routes/items.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+
+//Create
+router.get("/new", (req, res, next) => {
+  res.send("Form For Creating items");
+});
+
+router.post("/new", (req, res, next) => {
+  res.send("Make a new item, push to server, redirect to that page");
+});
+
+//Read
+router.get("/", (req, res, next) => {
+  res.send("Display a list of all items");
+});
+router.get("/:id", (req, res, next) => {
+  res.send("Find by id, display info of item");
+});
+
+//Update
+router.get("/update/:id", (req, res, next) => {
+  res.send(
+    "find by id, display same form as for new items, but prepopulate fields"
+  );
+});
+router.post("/update/:id", (req, res, next) => {
+  res.send("Find and update by id, redirect to page");
+});
+
+//Delete
+router.get("/delete/:id", (req, res, next) => {
+  res.send("display simple yes/no form for confirming delete, display info");
+});
+router.post("/delete/:id", (req, res, next) => {
+  res.send(
+    "display brief confirmation that the delete has gone through before redirecting to list of items"
+  );
+});
+
+module.exports;
