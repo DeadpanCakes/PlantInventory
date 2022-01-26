4685a8913da61e081b672c539a26fb7f22c82974
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-01-25 17:20:00 -0800

    imported itemRouter and used as middleware

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const categoryRouter = require("./routes/categories");
+const itemRouter = require("./routes/items");
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/categories", categoryRouter);
+app.use("/items", itemRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
