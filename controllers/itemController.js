471286e207e00aec351d615a3759857ad78ab23f
Anthony Mendoza <anthony.mendoza310@gmail.com>
2022-02-01 16:27:56 -0800

    replaced controller placeholders

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,35 +1,115 @@
+const async = require("async");
+
 const Item = require("../models/item");
+const Category = require("../models/category");
 
 //Create
 exports.getNew = (req, res, next) => {
-  res.send("Render empty item form");
+  Category.find().exec((err, categories) => {
+    if (err) next(err);
+    res.render("itemForm", {
+      title: "New Item",
+      item: {},
+      categories,
+      action: "/items/new",
+    });
+  });
 };
 exports.postNew = (req, res, next) => {
-  res.send("Push new item to db, redirect to that page");
+  Item.findOne({ name: req.body.itemName }).exec((err, existingItem) => {
+    if (err) next(err);
+    if (existingItem) {
+      res.redirect(existingItem.url);
+    } else {
+      const itemDetails = {
+        name: req.body.itemName,
+        desc: req.body.itemDesc,
+        price: req.body.itemPrice,
+        stock: req.body.itemStock,
+        categories: req.body.category,
+      };
+      console.log(req.body.itemName, " ", itemDetails);
+      const newItem = new Item(itemDetails);
+      newItem.save((err, item) => {
+        if (err) next(err);
+        res.redirect(item.url);
+      });
+    }
+  });
+  //res.send("Push new item to db, redirect to that page");
 };
 
 //Read
 exports.getList = (req, res, next) => {
-  res.send("Find render list of items");
+  Item.find().exec((err, items) => {
+    res.render("itemList", { title: "All Items", items });
+  });
 };
 exports.getItem = (req, res, next) => {
-  res.send("Find by id, display info");
+  Item.findById(req.params.id)
+    .populate("categories")
+    .exec((err, item) => {
+      if (err) next(err);
+      console.log(item);
+      res.render("itemDetails", { title: item.name, item });
+    });
 };
 
 //Update
 exports.getUpdate = (req, res, next) => {
-  res.send("Find by id and render item form pre-populated");
+  async.parallel(
+    {
+      item: (cb) => {
+        Item.findById(req.params.id).exec(cb);
+      },
+      categories: (cb) => {
+        Category.find().exec(cb);
+      },
+    },
+    (err, results) => {
+      if (err) next(err);
+      const { item, categories } = results;
+      res.render("itemForm", {
+        title: `Updating ${item.name}`,
+        item,
+        categories,
+        action: `/items/update/${item._id}`,
+      });
+    }
+  );
 };
 exports.postUpdate = (req, res, next) => {
-  res.send("Make new model, push to db, redirect to that page");
+  Category.findById(req.body.category).exec((err, category) => {
+    console.log(req.body);
+    if (err) next(err);
+    const itemDetails =  {
+      name: req.body.itemName,
+      desc: req.body.itemDesc,
+      price: req.body.itemPrice,
+      stock: req.body.itemStock,
+      categories: category,
+    };
+    Item.findByIdAndUpdate(req.params.id, itemDetails).exec((err, item) => {
+      if (err) next(err);
+      res.redirect(item.url);
+    });
+  });
+  //res.send("Make new model, push to db, redirect to that page");
 };
 
 //Delete
 exports.getDelete = (req, res, next) => {
-  res.send("Find by id, render confirmation page");
+  Item.findById(req.params.id).exec((err, item) => {
+    if (err) next(err);
+    res.render("itemDeletion", { title: `Delete ${item.name}?`, item });
+  });
+  //res.send("Find by id, render confirmation page");
 };
 exports.postDelete = (req, res, next) => {
-  res.send(
-    "Find by id and delet, redner page confirming delete, then redirect to item list"
-  );
+  Item.findByIdAndDelete(req.params.id).exec((err) => {
+    res.redirect("/items");
+  });
+  // res.send(
+  //   "Find by id and delet, redner page confirming delete, then redirect to item list"
+  // );
 };
